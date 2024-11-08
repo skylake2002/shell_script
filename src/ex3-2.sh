7a6e35edf5ff24162a7f5b7f9fad47e895d79ea0
skylake2002 <skylake_02@naver.com>
2024-11-08 23:33:08 +0900

    Update ex3-2.sh

--- src/ex3-2.sh
+++ src/ex3-2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 read num1 cal num2
-if [ $cal == '+' ]
-then
-    num3=expr $num1 + $num2
+if [ "$cal" == "+" ]; then
+    num3=$(expr $num1 + $num2)
 else
-    num3=expr $num1 - $num2
+    num3=$(expr $num1 - $num2)
 fi
 echo $num3
 exit 0
